8f32088da45932587c603cbb48ac5bbd82a2d79c
Ryan J. Geyer <me@ryangeyer.com>
2014-02-27 12:53:00 -0800

    Added yum-epel dependency and bump version

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,9 @@ maintainer_email "me@ryangeyer.com"
 license          "All rights reserved"
 description      "Installs/Configures rightscaleshim"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 supports "centos"
 
 depends "yum"
+depends "yum-epel"
